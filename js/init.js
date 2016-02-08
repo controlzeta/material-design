8a832c88d2efd5cc2ff4ae667b26f22d8cefe1d2
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-02-08 11:33:27 -0600

    Loader y menu flotante

--- js/init.js
+++ js/init.js
@@ -27,6 +27,46 @@ $("head").first().append('<script type="application/ld+json">' +
 	$('#fbURL').attr('href', fbURL);
 	$('#twURL').attr('href', twURL);
 	$('#insURL').attr('href', insURL);
+
+	    $('body').progressTracker({
+
+// Allows for navigating between content sections
+      linking: true,
+
+// "constant" = always visiable
+// "hover" = shows on mouse hover
+      tooltip: "hover",
+
+// The number specified is added to the default value at which the tracker changes to the next section.
+      positiveTolerance: 0,
+
+// The number specified is subtracted from the default value at which the tracker changes to the next section.
+      negativeTolerance: 0,
+
+// Only displays the progress tracker when the user is between the top of the first section and the bottom of the last;
+// It is only shown when the tracker sections are in view.
+// Specify false if you want the tracker to always show.
+      displayWhenActive: false,
+
+// Specify the value (in pixels) that you wish the progress tracker to be hidden when it is below that.
+      disableBelow: 0,
+
+// Specifies what the plugin takes into account when deciding when to switch to the next section.
+// "tracker" or "viewport"
+      tracking: "viewport"
+
+    });
+
+
+
+      /* Preloader */
+
+      setTimeout(function(){
+          jQuery('body').addClass('loaded');
+          jQuery('h1').css('color','#222222');
+      }, 3000);
+
+
   }); // end of document ready
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -36,5 +76,6 @@ $("head").first().append('<script type="application/ld+json">' +
 
   ga('create', 'UA-73301537-1', 'auto');
   ga('send', 'pageview');
+
 
 })(jQuery); // end of jQuery name space
