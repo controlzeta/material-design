2b6cecdd6fc5716dbe64b5110a29944b2b47b683
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-02-08 17:40:40 -0600

    Cambios en Galerias de Platillos

--- js/init.js
+++ js/init.js
@@ -2,27 +2,34 @@ var fbURL = "https://www.facebook.com/Mezzopane-1540279952936461/";
 var twURL = "https://www.facebook.com/Mezzopane-1540279952936461/";
 var insURL = "https://www.facebook.com/Mezzopane-1540279952936461/";
 
-$("head").first().append('<script type="application/ld+json">' +
-	'{ '+
-	  '"@context" : "http://schema.org",' +
-	  '"@type" : "LocalBusiness",' +
-	  '"name" : "mezzopane",' +
-	  '"image" : "http://40.118.247.246/mezzopane/img/mezzopane.png",' +
-	  '"telephone" : "23154654654",' +
-	  '"address" : {' +
-	    '"@type" : "PostalAddress",' +
-	    '"streetAddress" : "Filadelfia 128 Col. Nápoles",' +
-	    '"addressLocality" : "Ciudad de México"' +
-	  '}' +
-	'}' +
-	'</script>');
+// $("head").first().append('<script type="application/ld+json">' +
+// 	'{ '+
+// 	  '"@context" : "http://schema.org",' +
+// 	  '"@type" : "LocalBusiness",' +
+// 	  '"name" : "mezzopane",' +
+// 	  '"image" : "http://40.118.247.246/mezzopane/img/mezzopane.png",' +
+// 	  '"telephone" : "23154654654",' +
+// 	  '"address" : {' +
+// 	    '"@type" : "PostalAddress",' +
+// 	    '"streetAddress" : "Filadelfia 128 Col. Nápoles",' +
+// 	    '"addressLocality" : "Ciudad de México"' +
+// 	  '}' +
+// 	'}' +
+// 	'</script>');
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73301537-1', 'auto');
+  ga('send', 'pageview');
 
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
 	$('.materialboxed').materialbox();
 	$('#fbURL').attr('href', fbURL);
 	$('#twURL').attr('href', twURL);
@@ -68,14 +75,4 @@ $("head").first().append('<script type="application/ld+json">' +
 
 
   }); // end of document ready
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-73301537-1', 'auto');
-  ga('send', 'pageview');
-
-
 })(jQuery); // end of jQuery name space
