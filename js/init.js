f3e03c54669ee7e2ab7a5d868a766b954c534e2a
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-02-10 12:06:31 -0600

    Agregar el Loader y Scripts a todas las paginas

--- js/init.js
+++ js/init.js
@@ -20,7 +20,7 @@ var insURL = "https://www.facebook.com/Mezzopane-1540279952936461/";
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-73301537-1', 'auto');
   ga('send', 'pageview');
@@ -69,11 +69,11 @@ var insURL = "https://www.facebook.com/Mezzopane-1540279952936461/";
       setTimeout(function(){
           jQuery('body').addClass('loaded');
           jQuery('h1').css('color','#222222');
-      }, 10000);
+      }, 3000);
 
       setTimeout(function(){
         jQuery('#loader-wrapper').hide();
-      }, 15000); 
+      }, 4000); 
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
