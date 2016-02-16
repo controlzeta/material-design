dec18fff414bcfaf76a9bc2440441e35d269206c
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-02-15 18:07:49 -0600

    Formulario de Contacto

--- sendMail.php
+++ sendMail.php
@@ -3,9 +3,11 @@
 
 $destinatario = "francisco.arroyo@x1mexico.com";
 $asunto = "Contacto Mezzopane"; 
-$cuerpo = '<html><head><title>Mensaje desde Mezopanne.com</title></head><body><h1>Este correo llego a traves de mezzopane.com</h1><p>';
-$cuerpo .= "Asunto: " . $_POST['subject'] . " </ br>";
-$cuerpo .= "Comentarios: " . $_POST['comments'] . "</ br>";
+$cuerpo = '<html><head><title>Mensaje desde Mezopanne.com</title></head><body><h1>Este correo llego a traves de mezzopane.com</h1>';
+$cuerpo .= "<p>De: " . $_POST['email'] . " " . $_POST['first_name'] . " " . $_POST['last_name'] . "</p>";
+$cuerpo .= "<p>Asunto: " . $_POST['subject'] . " </p>";
+$cuerpo .= "<p>Comentarios: " . $_POST['comments'] . "</p>";
+
 $cuerpo .='</p></body></html>'; 
 
 
@@ -14,13 +16,13 @@ $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n"; 
 
 //dirección del remitente 
-$headers .= "From: Mezzopane <contacto@dmezzopane.com>\r\n"; 
+$headers .= "From: Web Mezzopane <" . $_POST['email'] . ">\r\n"; 
 
 //dirección de respuesta, si queremos que sea distinta que la del remitente 
-$headers .= "Reply-To: contacto@dmezzopane.com \r\n"; 
+$headers .= "Reply-To: " . $_POST['email'] . " \r\n"; 
 
 //ruta del mensaje desde origen a destino 
-$headers .= "Return-path: contacto@dmezzopane.com \r\n"; 
+$headers .= "Return-path: contacto@mezzopane.com \r\n"; 
 
 //direcciones que recibián copia 
 $headers .= "Cc: francisco.arroyo@x1mexico.com\r\n"; 
